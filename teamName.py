1a02ba5986853bce37304562b6c1bb7b29bb6190
gdubbz24 <73367258+gdubbz24@users.noreply.github.com>
2024-07-09 10:48:08 +1000

    mean reversion buy/sell size incl. mean PL of 10.2

--- teamName.py
+++ teamName.py
@@ -10,14 +10,45 @@ nInst = 50
 currentPos = np.zeros(nInst)
 
 
+# return position on day `i` after observing price on day `i`
+def mean_rev_pos(theo, prc, current_position):
+    # sell
+    if prc >= theo:
+        # got units to sell
+        if current_position:
+            current_position = 0
+        # no units previously bought.
+        # position is flat. hold as is.
+    # buy
+    else:
+        # buy in lots of 20
+        if current_position < 500:
+            current_position += 50
+        # reached 100. hold.
+
+    return current_position
+
+# pre-calculated "theoretical" means
+# I know the forecasted value should be E[X[t+1] | F[t]] where F[t] is vector of all prices up to time t
+# is: E[X[t+1] | F[t]] = a^(1)X_t 
+# in fact: E[X[t+k] | F[t]] = a^(k)X_t (where 'a' is the coefficient of X[t-1])
+# but tbh I think as a baseline -- exploiting mean reverting works fine
+
 def getMyPosition(prcSoFar):
+    # fair price of each instrument
+    fair_price = {
+        7: 46.73, # instrum 8
+        28: 51.16, # instrum 29
+        43: 60.92,
+        49: 56.04
+    }
+    last_prc = prcSoFar[:, -1]
+    
+    # positions for each instrument on current day
     global currentPos
-    (nins, nt) = prcSoFar.shape
-    if (nt < 2):
-        return np.zeros(nins)
-    lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])
-    lNorm = np.sqrt(lastRet.dot(lastRet))
-    lastRet /= lNorm
-    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:, -1]])
-    currentPos = np.array([int(x) for x in currentPos+rpos])
+
+    # update position
+    for i in [7, 28, 43, 49]:
+        currentPos[i] = mean_rev_pos(fair_price[i], last_prc[i], currentPos[i])
+    
     return currentPos
